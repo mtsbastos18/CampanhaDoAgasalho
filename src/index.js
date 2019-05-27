06f209a47b2bc23f256fb269a086c2eac628c870
Mateus <mtsbastos18@gmail.com>
2019-05-27 07:15:11 -0300

    mapa funcionando

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
-
+import { MapView } from 'react-native-maps'
 import Routes from './routes';
 
 const App = createAppContainer(Routes);
-
 
 export default App;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
-import { View, Text, StatusBar } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import MapView from 'react-native-maps'
 
-import {
-    Container,
-} from './styles';
+
 
 export default class Main extends Component {
     static navigationOptions = {
@@ -13,10 +12,46 @@ export default class Main extends Component {
 
     render() {
         return (
-            <Container>
-                <StatusBar hidden />
-                <Text>Começando o projeto </Text>
-            </Container>
-        )
+
+            <View style={styles.container}>
+
+                <MapView
+                    style={styles.map}
+                    loadingEnabled={true}
+                    region={{
+                        latitude: -30.036814,
+                        longitude: -51.216607,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+
+                </MapView>
+
+            </View>
+
+        );
     }
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+
+    },
+
+    map: {
+
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+
+    },
+
+});
