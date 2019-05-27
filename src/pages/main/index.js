5d103fc9b88f0bd37e1523403a8a4395c0ff9980
Mateus <mateus.bastos@anapps.org.br>
2019-05-27 14:42:38 -0300

    logo e geolocalizacao

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 
 import { View, StyleSheet, Text } from 'react-native';
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps'
+import { PermissionsAndroid } from 'react-native';
 
+import {
+    Container,
+    Titulo,
+    Logo
+} from './styles';
 
 
 export default class Main extends Component {
@@ -10,42 +16,103 @@ export default class Main extends Component {
         header: null,
     };
 
+    state = {
+
+        origin: { latitude: -30.036814, longitude: -51.216607 },
+    
+      };
+
+    async requestLocationPermission() {
+        try {
+    
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    'title': 'App Location Permission',
+                    'message': 'Maps App needs access to your map ' +
+                        'so you can be navigated.'
+                }
+            );
+    
+            if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                console.log("You can use the location");
+                return true;
+    
+            } else {
+                console.log("location permission denied");
+                return false;
+            }
+    
+        } catch (err) {
+            console.warn(err)
+        }
+    
+      }
+    
+      getLocation = () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            let newOrigin = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            };
+    
+            console.log('new origin');
+            console.log(newOrigin);
+    
+            this.setState({
+                origin: newOrigin
+            });
+        }, (err) => {
+            console.log('error');
+            console.log(err)
+    
+        }, {enableHighAccuracy: true, timeout: 2000})
+    
+      };
+
+      async componentDidMount() {
+        let isGranted = await this.requestLocationPermission();
+
+        if (isGranted) {
+            this.getLocation();
+        }
+
+        this.getLocation();
+
+      }
+
     render() {
         return (
 
-            <View style={styles.container}>
+            <Container>
+                <Logo source={require('../../images/logo2.png')} resizeMode="contain" />
 
                 <MapView
                     style={styles.map}
                     loadingEnabled={true}
                     region={{
-                        latitude: -30.036814,
-                        longitude: -51.216607,
+                        latitude: this.state.origin.latitude,
+                        longitude: this.state.origin.longitude,
                         latitudeDelta: 0.015,
                         longitudeDelta: 0.0121,
                     }}
                 >
-
+                    <Marker title={"Sua Localização"} 
+                        coordinate={{
+                            latitude: this.state.origin.latitude,
+                            longitude: this.state.origin.longitude,
+                        }}>
+                    </Marker>
                 </MapView>
 
-            </View>
+            </Container>
 
         );
     }
 }
 
 const styles = StyleSheet.create({
-
-    container: {
-
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'flex-end',
-
-    },
-
     map: {
-
         position: 'absolute',
         top: 0,
         left: 0,
@@ -53,5 +120,4 @@ const styles = StyleSheet.create({
         bottom: 0,
 
     },
-
 });
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -3,8 +3,19 @@ import styled from 'styled-components';
 const Container = styled.View`
   flex: 1;
   alignItems: center;
-  justifyContent: center;
-  backgroundColor: #F5F5F5;
 `;
 
-export {Container};
+const Titulo = styled.Text`
+  background-color: #fff;
+  font-size: 24px;
+`;
+const Logo = styled.Image`
+  width: 50%;
+  z-index: 9999;
+  position:absolute;
+  top:-130;
+  margin: 0;
+  padding: 0;
+`;
+
+export { Container, Titulo, Logo};
